35ddbc249e2d0b3d18f97db5b60d9e971035fef4
mac <2514407937@qq.com>
2021-12-23 19:45:55 +0800

    class07 code

--- class07/main.go
+++ class07/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"learn_go/class07/calc"
 
+	"github.com/spf13/cobra"
+	// learn_go_tools "learn.go.tools"
+
+)
+
+func main() {
+	// 录入
+	var (
+		name   string
+		sex    string
+		tall   float64
+		weight float64
+		age    int
+	)
+
+	cmd := &cobra.Command{
+		Use:   "healthcheck",
+		Short: "体脂计算器，根据身高、体重、性别、年龄计算体脂比，并给出健康建议",
+		Long:  "该体脂计算器是基于BMI的体脂计算器。。。。。。。",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("name: ", name)
+			fmt.Println("sex: ", sex)
+			fmt.Println("tall: ", tall)
+			fmt.Println("weight: ", weight)
+			fmt.Println("age: ", age)
+			// 计算
+			bmi := calc.CalcBMI(tall, weight)
+			fatRate := calc.CalcBodyFatRatio(sex, age, bmi)
+			fmt.Println("fatRate: ", fatRate)
+			// 评估结果
+			// todo
+		},
+	}
+	cmd.Flags().StringVar(&name, "name", "", "姓名")
+	cmd.Flags().StringVar(&sex, "sex", "", "性别")
+	cmd.Flags().Float64Var(&tall, "tall", 0, "身高")
+	cmd.Flags().Float64Var(&weight, "weight", 0, "体重")
+	cmd.Flags().IntVar(&age, "age", 0, "年龄")
+
+	cmd.Execute()
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,10 @@ module learn_go
 
 go 1.17
 
+require github.com/spf13/cobra v1.3.0
+
 require (
-	github.com/armstrongli/go-bmi v0.0.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/spf13/cobra v1.3.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
 
